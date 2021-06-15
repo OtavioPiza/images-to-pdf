d8bcc707b729e3e1406b56c51839f1bc7f5a7f94
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2021-06-14 21:48:02 -0300

    moved scanning function to external files

--- src/converter.py
+++ src/converter.py
@@ -9,8 +9,9 @@ contains the functions necessary to create a pdf file from images
 # imports #
 # ======= #
 
-from os import scandir, DirEntry, walk
-from typing import List, NoReturn, Tuple, Dict
+from typing import List, NoReturn
+
+from src.image_scaner import get_images
 
 
 # ======= #
@@ -29,7 +30,7 @@ class PDF:
         """
         self.pages = 0
         self.pdf_path = 'pdf' if not pdf_path else pdf_path
-        self.images = self.get_images(images_paths)
+        self.images = get_images(images_paths)
 
     def __len__(self) -> int:
         """
@@ -38,30 +39,6 @@ class PDF:
         :return: number of pages in the document
         """
         return self.pages
-
-    # == static methods == #
-
-    @staticmethod
-    def images_in_folder(path: str) -> List[str]:
-        """
-        returns all the images in a folder
-
-        :param path: path to images
-        :return:
-        """
-        img: DirEntry
-        return [img.name for img in scandir(path) if img.name.split('.')[-1] in 'jpg jpeg png']
-
-    @staticmethod
-    def get_images(paths: List[str]) -> Dict[str, List[str]]:
-        """
-        returns a dict with all images in the paths provided
-
-        :return:
-        """
-        folder: Tuple[str, List[str], List[str]]
-        return {path[0]: PDF.images_in_folder(path[0]) for image_path in paths
-                for path in walk(image_path) if PDF.images_in_folder(path[0])}
 
 
 if __name__ == '__main__':
