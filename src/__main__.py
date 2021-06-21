9a2035e5870f3ac9a2b2785c43e17ed905e67f6b
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2021-06-21 13:29:00 -0300

    added gui files

--- src/__main__.py
+++ src/__main__.py
@@ -1,2 +1,34 @@
-if __name__ == '__main__':
-    print('test')
+from tkinter import Tk, X, N
+from typing import NoReturn
+
+
+class App:
+
+    def __init__(self):
+        """
+        constructor
+        """
+        self.root = Tk()
+        self.root.geometry('200X150')
+        self.root.title('Images to PDF')
+        self.center = None
+
+    # == methods == #
+
+    def update_center(self, mode: str = 'main') -> NoReturn:
+        """
+        updates the center depending on the mode provided
+
+        :param mode: mode
+        """
+        if self.center:
+            self.center.destroy()
+
+        if mode == 'main':
+            pass
+
+        else:
+            pass
+
+        self.center.pack(padx=10, pady=10, fill=X, anchor=N)
+
