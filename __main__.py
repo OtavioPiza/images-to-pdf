4d03a8e215da98a0d6ac5fc264841fdd39ea7ec1
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2021-06-28 19:04:06 -0300

    added status message

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
-from tkinter import Tk
+from tkinter import Tk, messagebox
 from tkinter.filedialog import askdirectory
 
 from src.converter import PDF
@@ -22,6 +22,8 @@ class App:
         path_to_images = askdirectory(title='Select a folder with the images', initialdir='~')
         pdf = PDF(path_to_images.split('/')[-1], [path_to_images], '/'.join(path_to_images.split('/')[:-1]))
         pdf.create_pdf()
+
+        messagebox.showinfo('Images to PDF', 'PDF created')
 
 
 App()
