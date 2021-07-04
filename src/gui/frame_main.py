508ce5eda2b380cd590e431674b52fc45c115aab
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2021-07-04 16:35:13 -0300

    improved docs

--- src/gui/frame_main.py
+++ src/gui/frame_main.py
@@ -7,16 +7,26 @@ from src.converter import PDF
 
 
 class MainFrame(LabelFrame):
+    """
+    main frame for the images to pdf application
+    """
 
     def __init__(self, root: Tk, do_reset: Callable):
-        LabelFrame.__init__(self, root)
+        """
+        :param root: tkinter root
+        :param do_reset: function to reset the application
+        """
+        self.do_reset = do_reset
 
+        # == create window == #
+
+        LabelFrame.__init__(self, root)
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
         self.rowconfigure(1, weight=1)
         self.rowconfigure(2, weight=1)
 
-        self.do_reset = do_reset
+        # == add widgets == #
 
         self.bt = Button(self, text='Create PDF', command=self.create_pdf)
         self.bt.grid(row=0, column=0, sticky='news')
@@ -32,17 +42,35 @@ class MainFrame(LabelFrame):
         self.label.grid(row=2, column=0, sticky='news')
 
     def create_pdf(self) -> NoReturn:
+        """
+        creates a new pdf object with the directory provided by the user
+        """
+        # == ask for directory == #
+
         path_to_images = askdirectory(title='Select a folder with the images', initialdir='~')
+
+        if not path_to_images:
+            return
+
+        # == create pdf == #
 
         pdf = PDF(path_to_images.split('/')[-1], [path_to_images], '/'.join(path_to_images.split('/')[:-1]), '')
         pdf.add_pages(self.increase_progress_bar)
+
         self.ps_text.set('Saving the PDF...')
         self.update()
+
         pdf.create_pdf()
         messagebox.showinfo('Images to PDF', message='PDF created')
+
         self.do_reset()
 
     def increase_progress_bar(self, i: int):
+        """
+        increases the progress bar by i 
+
+        :param i: progress bar increment
+        """
         self.pb['value'] += i
         self.ps_text.set(f'Scanning images: {self.pb["value"]:.2f}% complete')
         self.update()
