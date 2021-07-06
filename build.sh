3369278859b60d31d59d847ded5ca642fefcbf22
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2021-07-05 21:44:15 -0300

    updated build script

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-pyinstaller __main__.py --name images-to-pdf --onefile
-mv ./dist/images-to-pdf ./dist
-rm -r -f -d dist build
-rm images-to-pdf.spec
+pyinstaller __main__.py --name images-to-pdf.out --onefile
+rm -r -f -d build
